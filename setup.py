b3aabc4fc8481ad858444b9c8a352297a4ad0e8c
Trevor Lee <31684028+trevorwslee@users.noreply.github.com>
2022-03-04 18:54:40 +0800

    commit

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
   long_description = fh.read()
 
 setuptools.setup(
-  name='dumbdisplay',
+  name='uDumbDisplayLib',
   version='0.0.1',
   author='Trevor Lee',
   author_email='trev_lee@hotmail.com',
