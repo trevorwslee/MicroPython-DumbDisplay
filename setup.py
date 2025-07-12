eb29f594b28a6623bc501d7b5dae423652870e6e
Trevor Lee <trevorwslee@gmail.com>
2025-07-12 12:23:43 +0800

    0.3.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
   name='uDumbDisplayLib',
-  version='0.3.1',
+  version='0.3.5',
   author='Trevor Lee',
   author_email='trevorwslee@gmail.com',
   description='MicroPython DumbDisplay Library',
