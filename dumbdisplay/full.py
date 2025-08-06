77d12d20fae46cdf779bd1f302000b9094861561
Trevor Lee <trevorwslee@gmail.com>
2025-08-06 21:49:41 +0800

    updated

--- dumbdisplay/full.py
+++ dumbdisplay/full.py
@@ -5,9 +5,10 @@
 from .core import *
 from .ios import *
 from .layer_7segrow import *
+from .layer_graphical import *
 from .layer_lcd import *
 from .layer_ledgrid import *
-from .layer_graphical import *
+from .layer_plotter import *
 from .layer_selection import *
 
 from ._exp_tunnel_basic import *
