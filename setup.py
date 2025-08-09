c33bb9c52d9d1232defceb37641488147d01e341
Trevor Lee <trevorwslee@gmail.com>
2025-08-09 21:48:06 +0800

    changed setup.py

--- setup.py
+++ setup.py
@@ -1,23 +1,14 @@
 import setuptools
 
 
-# read the contents of your README file
-from pathlib import Path
-this_directory = Path(__file__).parent
-long_description = (this_directory / "README.md").read_text()
-
 setuptools.setup(
   name='uDumbDisplayLib',
   version='0.5.0',
   author='Trevor Lee',
   author_email='trevorwslee@gmail.com',
   description='MicroPython DumbDisplay Library',
-  long_description=long_description,
   long_description_content_type="text/markdown",
   url='https://github.com/trevorwslee/MicroPython-DumbDisplay',
-  project_urls = {
-  },
   license='MIT',
   packages=["dumbdisplay", "dumbdisplay_examples"],
-  install_requires=[],
 )
